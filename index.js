38014c63977b29e2e56744d9c8036b65faebf195
Anna-Marie Moss <ambrown1976@googlemail.com>
2021-01-27 17:17:52 +0000

    about us page

--- index.js
+++ index.js
@@ -34,4 +34,47 @@ new TypeIt('#type1', {
   .type('Developer', { delay: 400 })
   .pause(500)
   .delete(9)
+  .type('Project Mgr', { delay: 400 })
+  .pause(500)
+  .delete(9)
   .go();
+
+// animated job roles text for "About me" page
+new TypeIt('#type2', {
+  speed: 120,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type('Designer', { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .type('Developer', { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .type('Project Mgr', { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .go();
+
+// gsap - javascript web animation on the "Software Developer Title at top of page"
+gsap.from('.logo', { opacity: 0, duration: 1, delay: 0.5, y: -10 }); // slides down
+gsap.from('.hamburger', { opacity: 0, duration: 1, delay: 1, x: 20 }); // slides from righthand side
+gsap.from('.banner', { opacity: 0, duration: 1, delay: 1.5, x: -200 }); // slide from lefthand side
+gsap.from('.hero h3', { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from('.hero h1', { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from('.hero h4', { opacity: 0, duration: 1, delay: 3, y: -30 });
+gsap.from('.hero a', { opacity: 0, duration: 1, delay: 3.5, y: 50 }); //slides up
+gsap.from('.nav-item', {
+  opacity: 0,
+  duration: 1,
+  delay: 1.2,
+  y: 30,
+  stagger: 0.2,
+}); //makes nav links ripple up one after the other
+gsap.from('.icons span', {
+  opacity: 0,
+  duration: 1,
+  delay: 4,
+  x: -30,
+  stagger: 0.2,
+}); //ripples in from left
