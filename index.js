7ca3acfad2d538692581448dbca520d2921b2faa
Anna-Marie Moss <ambrown1976@googlemail.com>
2021-01-29 13:57:52 +0000

    tweak

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ new TypeIt('#type1', {
   .type('Developer', { delay: 400 })
   .pause(500)
   .delete(9)
-  .type('Project Mgr', { delay: 400 })
+  .type('Project Manager', { delay: 400 })
   .pause(500)
   .delete(9)
   .go();
